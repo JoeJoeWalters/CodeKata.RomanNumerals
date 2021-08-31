05546a5efc796667d516f5bef9886b6174d48542
joe walters <joewalters999@gmail.com>
2021-08-31 23:35:26 +0100

    Expanded to multuple numerals

--- CodeKata.RomanNumerals/Core/RomanNumeralConverter.cs
+++ CodeKata.RomanNumerals/Core/RomanNumeralConverter.cs
@@ -5,15 +5,15 @@ namespace Core
 {
     public class RomanNumeralConverter
     {
-        private Dictionary<string, int> numerals = new Dictionary<string, int>()
+        private Dictionary<char, int> numerals = new Dictionary<char, int>()
         { 
-            { "I", 1 },
-            { "V", 5 },
-            { "X", 10 },
-            { "L", 50 },
-            { "C", 100 },
-            { "D", 500 },
-            { "M", 1000 }
+            { 'I', 1 },
+            { 'V', 5 },
+            { 'X', 10 },
+            { 'L', 50 },
+            { 'C', 100 },
+            { 'D', 500 },
+            { 'M', 1000 }
         };
 
         public RomanNumeralConverter()
@@ -21,14 +21,31 @@ namespace Core
         }
 
         public string FromDigit(int digit)
+            => GetNumeral(digit);
+
+        private string GetNumeral(int digit)
         {
             if (numerals.ContainsValue(digit))
-                return numerals.Where(x => x.Value == digit).Select(x => x.Key).FirstOrDefault();
+                return numerals.Where(x => x.Value == digit).Select(x => x.Key).FirstOrDefault().ToString();
             else
                 return string.Empty;
         }
 
-        public int FromNumeral(string numeral)
+        public int FromNumerals(string numerals)
+        {
+            int rollingValue = 0;
+            int lastValue = 0;
+            for (int position = 0; position < numerals.Length; position ++)
+            {
+                int value = GetDigit(numerals[position]);
+                rollingValue += value;
+                lastValue = value;
+            }
+
+            return rollingValue;
+        }
+
+        private int GetDigit(char numeral)
         {
             if (numerals.ContainsKey(numeral))
                 return numerals[numeral];
--- CodeKata.RomanNumerals/Tests/Tests.cs
+++ CodeKata.RomanNumerals/Tests/Tests.cs
@@ -24,16 +24,39 @@ namespace Tests
         [InlineData(100, "C")]
         [InlineData(500, "D")]
         [InlineData(1000, "M")]
-        public void Value_To_Numeral_Equals_ExpectedValue(int digit, string numeral)
+        public void Digit_To_Numeral_Equals_ExpectedValue(int digit, string numeral)
         {
             // ARRANGE
             var thing = new RomanNumeralConverter();
 
             // ACT
-            string result = converter.FromDigit(digit);
+            int fromNumeral = converter.FromNumerals(numeral);
 
             // ASSERT
-            result.Should().Be(numeral);
+            fromNumeral.Should().Be(digit);
+        }
+
+        [Theory]
+        [InlineData(1, "I")]
+        [InlineData(2, "II")]
+        [InlineData(3, "III")]
+        [InlineData(4, "IV")]
+        [InlineData(5, "V")]
+        [InlineData(10, "X")]
+        [InlineData(50, "L")]
+        [InlineData(100, "C")]
+        [InlineData(500, "D")]
+        [InlineData(1000, "M")]
+        public void Numeral_To_Digit_Equals_ExpectedValue(int digit, string numeral)
+        {
+            // ARRANGE
+            var thing = new RomanNumeralConverter();
+
+            // ACT
+            string fromDigit = converter.FromDigit(digit);
+
+            // ASSERT
+            fromDigit.Should().Be(numeral);
         }
     }
 }
