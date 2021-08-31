cb4ce6003498a730e635011512435f88c779e5e3
joe walters <joewalters999@gmail.com>
2021-08-31 23:23:23 +0100

    Basic conversion with single numerals to digits

--- CodeKata.RomanNumerals/Core/RomanNumeralConverter.cs
+++ CodeKata.RomanNumerals/Core/RomanNumeralConverter.cs
@@ -1,21 +1,39 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace Core
 {
     public class RomanNumeralConverter
     {
+        private Dictionary<string, int> numerals = new Dictionary<string, int>()
+        { 
+            { "I", 1 },
+            { "V", 5 },
+            { "X", 10 },
+            { "L", 50 },
+            { "C", 100 },
+            { "D", 500 },
+            { "M", 1000 }
+        };
+
         public RomanNumeralConverter()
         {
         }
 
         public string FromDigit(int digit)
         {
-            return string.Empty;
+            if (numerals.ContainsValue(digit))
+                return numerals.Where(x => x.Value == digit).Select(x => x.Key).FirstOrDefault();
+            else
+                return string.Empty;
         }
 
         public int FromNumeral(string numeral)
         {
-            return 0;
+            if (numerals.ContainsKey(numeral))
+                return numerals[numeral];
+            else
+                return 0;
         }
     }
 }
